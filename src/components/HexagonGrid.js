ada7d005d0df05ca62ec38b0575f2f8ba9e2c5e7
Hagar Shilo <strayblues@gmail.com>
2019-08-13 22:36:03 +0300

    WIP

--- src/components/HexagonGrid.js
+++ src/components/HexagonGrid.js
@@ -10,14 +10,37 @@ const HexagonGrid = () => {
 
   return (
     <Container>
-      <svg height="210" width="500">
-        <polygon
-          points="50,25 100,25 125,68 100,111 50,111 25,68"
-          style={hexagonStyle}
-        />
+      <svg viewBox="0 0 1500 1500">
+        <defs>
+          <polygon
+            id="hexagon"
+            points="50,25 100,25 125,68 100,111 50,111 25,68"
+            style={hexagonStyle}
+          />
+        </defs>
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((object, i) => (
+          <use xlinkHref="#hexagon" transform={"translate(0," + i * 86 + ")"} />
+        ))}
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((object, i) => (
+          <use
+            xlinkHref="#hexagon"
+            transform={"translate(75," + (i * 86 + 43) + ")"}
+          />
+        ))}
+        {/* <use xlinkHref="#hexagon" transform="translate(0, 86)" />
+        <use xlinkHref="#hexagon" transform="translate(0, 172)" />
+        <use xlinkHref="#hexagon" transform="translate(0, 258)" /> */}
+        <use xlinkHref="#hexagon" transform="translate(75, 43)" />
+        <use xlinkHref="#hexagon" transform="translate(75, 129)" />
+        <use xlinkHref="#hexagon" transform="translate(75, 215)" />
+        <use xlinkHref="#hexagon" transform="translate(75, 301)" />
+        <use xlinkHref="#hexagon" transform="translate(150, 0)" />
+        <use xlinkHref="#hexagon" transform="translate(150, 86)" />
+        <use xlinkHref="#hexagon" transform="translate(225, 43)" />
+        <use xlinkHref="#hexagon" transform="translate(225, 129)" />
+        <use xlinkHref="#hexagon" transform="translate(300, 86)" />
         Sorry, your browser does not support inline SVG.
       </svg>
-      {/* <svg width={window.innerWidth} height={window.innerHeight} />; */}
     </Container>
   );
 };
