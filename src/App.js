ba0b71281449d394444a72b0dd499b4ea4d2f736
Hagar Shilo <strayblues@gmail.com>
2019-08-13 13:12:32 +0300

    Initial cleanup and setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Content>
+          <Header />
+          <Sidebar />
+          <Grid>
+            <svg height="210" width="500">
+              <polygon
+                points="150,10 100,40 100,90 150,120 200,90 200,40"
+                style="fill:lime;stroke:purple;stroke-width:1"
+              />
+              Sorry, your browser does not support inline SVG.
+            </svg>
+          </Grid>
+        </Content>
+        <Footer />
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
+
+const Container = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background: #e0e0e0;
+  display: flex;
+  flex-direction: column;
+`;
+const Content = styled.div`
+  flex: 1 0 auto;
+`;
+const Header = styled.div``;
+const Sidebar = styled.div``;
+const Grid = styled.div``;
+const Footer = styled.div``;
