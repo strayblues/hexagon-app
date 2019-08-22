bfb26212d59d5cf400fbc3b7d55acbbf813164c0
Hagar Shilo <strayblues@gmail.com>
2019-08-22 18:13:50 +0300

    Updates

---
+++ precache-manifest.d4be5d9cbc471a3de7fe67706e321f9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cad6dbe3825033d8860997aa595ebc3",
+    "url": "/hexagon-app/index.html"
+  },
+  {
+    "revision": "722581241e0eb80d032e",
+    "url": "/hexagon-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f2a86c9311e9878f613f",
+    "url": "/hexagon-app/static/js/2.1b2d5e4d.chunk.js"
+  },
+  {
+    "revision": "722581241e0eb80d032e",
+    "url": "/hexagon-app/static/js/main.dbb15bf6.chunk.js"
+  },
+  {
+    "revision": "80323d81d337004cb467",
+    "url": "/hexagon-app/static/js/runtime~main.486a813d.js"
+  }
+]);
