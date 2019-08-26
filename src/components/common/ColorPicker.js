da44526399df587a054229238fdb9ab633005bf7
Hagar Shilo <strayblues@gmail.com>
2019-08-26 19:51:12 +0300

    Add color selection functionality

--- src/components/common/ColorPicker.js
+++ src/components/common/ColorPicker.js
@@ -5,32 +5,28 @@ class ColorPicker extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: "pink",
-      isToggleOn: true
+      currentColor: "pink"
     };
-
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
+  handleClick = e => {
+    this.setState({
+      currentColor: e.target.attributes.color.value
+    });
+  };
 
   render() {
     return (
       <Container>
-        <Yellow onClick={this.handleClick} />
-        <Green onClick={this.handleClick} />
-        <Red onClick={this.handleClick}>
-          {this.state.isToggleOn ? "ON" : "OFF"}
-        </Red>
-        <Black onClick={this.handleClick} />
-        <Blue onClick={this.handleClick} />
-        <Violet onClick={this.handleClick} />
-        <Orange onClick={this.handleClick} />
-        <Clear onClick={this.handleClick} />
+        <Color color="gold" onClick={this.handleClick} />
+        <Color color="limegreen" onClick={this.handleClick} />
+        <Color color="crimson" onClick={this.handleClick} />
+        <Color color="black" onClick={this.handleClick} />
+        <Color color="dodgerblue" onClick={this.handleClick} />
+        <Color color="violet" onClick={this.handleClick} />
+        <Color color="orange" onClick={this.handleClick} />
+        <Clear color="white" onClick={this.handleClick} />
+        <CurrentColor color={this.state.currentColor} />
       </Container>
     );
   }
@@ -51,12 +47,12 @@ const Container = styled.div`
   cursor: pointer;
 `;
 const Color = styled.button`
-  width: 40px;
-  height: 40px;
-  margin: 0 7px;
+  background: ${props => props.color};
+  width: 3em;
+  height: 3em;
+  margin: 0 0.5em;
   border-radius: 50%;
   border: 1px solid black;
-  background: pink;
   outline: none;
   transition: all 300ms;
 
@@ -64,28 +60,9 @@ const Color = styled.button`
     box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
   }
 `;
-const Red = styled(Color)`
-  background: crimson;
-`;
-const Yellow = styled(Color)`
-  background: gold;
-`;
-const Green = styled(Color)`
-  background: limegreen;
-`;
-const Blue = styled(Color)`
-  background: dodgerblue;
-`;
-const Violet = styled(Color)`
-  background: violet;
-`;
-const Black = styled(Color)`
-  background: black;
-`;
-const Orange = styled(Color)`
-  background: orange;
-`;
 const Clear = styled(Color)`
-  background: white;
+  border-radius: 0%;
+`;
+const CurrentColor = styled(Color)`
   border-radius: 0%;
 `;
