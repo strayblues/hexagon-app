d851c68564dfbdeed29f9d1701e88637f469a554
Hagar Shilo <strayblues@gmail.com>
2019-08-27 18:11:54 +0300

    WIP

--- src/components/HexagonEmptyBoard.js
+++ src/components/HexagonEmptyBoard.js
@@ -1,47 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 
-const HexagonEmptyBoard = () => {
-  let hexagonStyle = {
-    fill: "white",
-    stroke: "black",
-    "stroke-width": "2"
+class HexagonEmptyBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hexagonStyle: {
+        fill: "white",
+        stroke: "black",
+        "stroke-width": "2"
+      }
+    };
+  }
+
+  handleClick = e => {
+    this.setState({
+      hexagonStyle: {
+        fill: "red"
+      }
+    });
   };
 
-  return (
-    <Container>
-      <svg viewBox="0 0 1350 770">
-        <defs>
-          <polygon
-            id="hexagon"
-            points="50,25 100,25 125,68 100,111 50,111 25,68"
-            style={hexagonStyle}
-          />
-        </defs>
-        {[0, 1, 2, 3, 4, 5, 6, 7].map(i => {
-          return [0, 2, 4, 6, 8, 10, 12, 14].map(j => {
-            return (
-              <>
-                <use
-                  xlinkHref="#hexagon"
-                  transform={"translate(" + j * 75 + "," + i * 86 + ")"}
-                  // style={{ fill: this.state.colorArray[i] }}
-                />
-                <use
-                  xlinkHref="#hexagon"
-                  transform={
-                    "translate(" + (j + 1) * 75 + "," + (i * 86 + 43) + ")"
-                  }
-                />
-              </>
-            );
-          });
-        })}
-        Sorry, your browser does not support inline SVG.
-      </svg>
-    </Container>
-  );
-};
+  render() {
+    return (
+      <Container>
+        <svg viewBox="0 0 1350 770">
+          <defs>
+            <polygon
+              id="hexagon"
+              points="50,25 100,25 125,68 100,111 50,111 25,68"
+              style={this.state.hexagonStyle}
+            />
+          </defs>
+          {[0, 1, 2, 3, 4, 5, 6, 7].map(i => {
+            return [0, 2, 4, 6, 8, 10, 12, 14].map(j => {
+              return (
+                <>
+                  <use
+                    onClick="handleClick"
+                    xlinkHref="#hexagon"
+                    transform={"translate(" + j * 75 + "," + i * 86 + ")"}
+                  />
+                  <use
+                    onClick="handleClick"
+                    xlinkHref="#hexagon"
+                    transform={
+                      "translate(" + (j + 1) * 75 + "," + (i * 86 + 43) + ")"
+                    }
+                  />
+                </>
+              );
+            });
+          })}
+          Sorry, your browser does not support inline SVG.
+        </svg>
+      </Container>
+    );
+  }
+}
 
 export default HexagonEmptyBoard;
 
