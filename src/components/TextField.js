a508d16f03979c9a725f62b2e8267c267d5f8471
Hagar Shilo <strayblues@gmail.com>
2019-09-10 14:27:36 +0300

    Start work on conditional line numbering

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import Input from "./common/Input";
 
 class TextField extends Component {
-  state = {};
+  state = {
+    isEnter: false
+  };
 
-  // TODO: functions: handleInputChange, addInput, etc.
+  trackEnter = e => {
+    if (e.key === "Enter") {
+      alert("enter pressed");
+      this.setState({ isEnter: true });
+    }
+  };
+
+  // TODO: functions: handleInputChange/submit, addInputToData, etc.
 
   render() {
     return (
@@ -13,11 +21,11 @@ class TextField extends Component {
         <Container>
           <Instructions />
           <InputContainer>
-            <StyledInput placeholder="Start typing here...">
-              {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((a, i) => (
-                  <textarea placeholder="input..." label={i + 1} />
-              ))} */}
-            </StyledInput>
+            <LineNumbering>1</LineNumbering>
+            <DescriptionInput
+              onKeyUp={this.trackEnter}
+              placeholder="Start typing here..."
+            ></DescriptionInput>
           </InputContainer>
         </Container>
       </>
@@ -29,9 +37,12 @@ export default TextField;
 
 const Container = styled.div``;
 const Instructions = styled.div``;
-
-const StyledInput = styled.textarea`
-  width: 95%;
+const InputContainer = styled.div`
+  height: 80vh;
+  display: flex;
+`;
+const LineNumbering = styled.textarea`
+  width: 7%;
   height: 88%;
   margin: 4px;
   background-color: #f5f5f5;
@@ -51,9 +62,40 @@ const StyledInput = styled.textarea`
     linear-gradient(right, white 10px, transparent 10px),
     linear-gradient(white 30px, #ccc 30px, #ccc 31px, white 31px);
   background-size: 100% 100%, 100% 100%, 100% 31px;
-  border: 1px solid #ccc;
+  line-height: 31px;
+  font-size: 18px;
+  color: #666;
+  border-color: white;
+  padding: 8px;
+  &:focus {
+    outline: none;
+  }
+`;
+const DescriptionInput = styled.textarea`
+  width: 100%;
+  height: 88%;
+  margin: 4px;
+  background-color: #f5f5f5;
+  background-image: -webkit-linear-gradient(left, white 10px, transparent 10px),
+    -webkit-linear-gradient(right, white 10px, transparent 10px),
+    -webkit-linear-gradient(white 30px, #ccc 30px, #ccc 31px, white 31px);
+  background-image: -moz-linear-gradient(left, white 10px, transparent 10px),
+    -moz-linear-gradient(right, white 10px, transparent 10px),
+    -moz-linear-gradient(white 30px, #ccc 30px, #ccc 31px, white 31px);
+  background-image: -ms-linear-gradient(left, white 10px, transparent 10px),
+    -ms-linear-gradient(right, white 10px, transparent 10px),
+    -ms-linear-gradient(white 30px, #ccc 30px, #ccc 31px, white 31px);
+  background-image: -o-linear-gradient(left, white 10px, transparent 10px),
+    -o-linear-gradient(right, white 10px, transparent 10px),
+    -o-linear-gradient(white 30px, #ccc 30px, #ccc 31px, white 31px);
+  background-image: linear-gradient(left, white 10px, transparent 10px),
+    linear-gradient(right, white 10px, transparent 10px),
+    linear-gradient(white 30px, #ccc 30px, #ccc 31px, white 31px);
+  background-size: 100% 100%, 100% 100%, 100% 31px;
+  /* border: 1px solid #ccc; */
+  border-color: white;
   border-radius: 8px;
-  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1);
+  /* box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.1); */
   line-height: 31px;
   font-size: 18px;
   color: #666;
@@ -61,7 +103,4 @@ const StyledInput = styled.textarea`
   &:focus {
     outline: none;
   }
-`;
-const InputContainer = styled.div`
-  height: 80vh;
 `;
